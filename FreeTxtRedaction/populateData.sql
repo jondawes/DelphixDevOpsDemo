c70bbb4c49feca43ef257bc3dd7bb6285cf32ab4
Jon Dawes <jon.dawes@delphix.com>
2023-01-16 16:13:43 +1100

    added free text redaction files

---
+++ FreeTxtRedaction/populateData.sql
@@ -0,0 +1,23 @@
+update contacts
+set description='SSN is 123-45-6789   tax amount $456.78'
+where id='116388bb-c44f-23eb-da08-5ba11bab0a2d';
+
+update contacts
+set description='phone is 123-456-7890'
+where id='11e10731-10a1-f76b-46a0-5ba11beeb333';
+
+update contacts
+set description='email is elon.musk@tesla.com'
+where id='15012503-a62c-539d-b069-5ba11bba46a0';
+
+update contacts
+set description='Account number is R34567-HK'
+where id='16a8df73-5e8e-18d8-0b18-5ba11b057df5';
+
+update contacts
+set description='Vehicle Reg are WTD-1234-Z and D-23-SW'
+where id='1bb7b27a-97a1-5567-70f2-5ba11b4c9a94';
+
+update contacts
+set description='Address 855 Main Street, Redwood City, CA 94063'
+where id='1ce7f1f1-7ecb-a409-b5c7-5ba11baa833d';
